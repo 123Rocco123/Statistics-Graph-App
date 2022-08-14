029807789b8d260a57399277817cbdadaa74b7bf
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-08-14 16:57:51 +0100

    Imports

--- mainClass.java
+++ mainClass.java
@@ -1,3 +1,9 @@
+// GUI Imports
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
 // mainClass Class is used to contain all of the other class calls
 class mainClass {
   public mainClass() {
