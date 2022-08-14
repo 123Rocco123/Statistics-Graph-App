7fa3362437aa65651264d032c84d5f49f9f33780
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-08-14 17:17:24 +0100

    JFrame Declaration and Settings

--- mainClass.java
+++ mainClass.java
@@ -6,7 +6,14 @@ import java.awt.event.*;
 
 // mainClass Class is used to contain all of the other class calls
 class mainClass {
+  JFrame mainFrame = new JFrame();
+
   public mainClass() {
-    
+    // JFrame Settings
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mainFrame.setSize(500, 500);
+    mainFrame.setLocationRelativeTo(null);
+    mainFrame.setTitle("Analysis Program");
+    mainFrame.setVisible(true);
   }
 }
