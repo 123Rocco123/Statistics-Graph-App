9c5ce6ffbb65ce5ecd918568120bda362f8b78c7
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-08-15 02:10:37 +0100

    Initial commit

---
+++ buttonPanel.java
@@ -0,0 +1,14 @@
+// GUI Imports
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class buttonPanel {
+  JPanel buttonPanelObj = new JPanel();
+
+  public buttonPanel() {
+    buttonPanelObj.setBounds(10, 310, 465, 140);
+    buttonPanelObj.setBackground(Color.BLACK);
+  }
+}
