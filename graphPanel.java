10744e721be555172a02274aaa5c20bf0e2e4951
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-08-15 02:10:44 +0100

    Initial Commit

---
+++ graphPanel.java
@@ -0,0 +1,14 @@
+// GUI Imports
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class graphPanel {
+  JPanel graphPanel = new JPanel();
+
+  public graphPanel() {
+    graphPanel.setBounds(10, 10, 465, 290);
+    graphPanel.setBackground(Color.BLACK);
+  }
+}
