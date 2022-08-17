0880f5d6a9fb88834eb675e1182f4baa55bf82bf
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-08-17 18:58:59 +0100

    JButton Instance Variables

--- buttonPanel.java
+++ buttonPanel.java
@@ -7,6 +7,12 @@ import java.awt.event.*;
 class buttonPanel {
   JPanel buttonPanelObj = new JPanel();
 
+  JButton insertData = new JButton("Insert Data");
+  JButton displayData = new JButton("Display Graph");
+
+  JButton zoomIn = new JButton("Zoom In");
+  JButton zoomOut = new JButton("Zoom Out");
+
   public buttonPanel() {
     buttonPanelObj.setBounds(10, 310, 465, 140);
     buttonPanelObj.setBackground(Color.BLACK);
