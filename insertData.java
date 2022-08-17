969bd376994d3e0bf40ca6c14248b8671fcd6e43
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-08-17 19:12:41 +0100

    Instance Variables

--- insertData.java
+++ insertData.java
@@ -4,3 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+  JTextField xValues = new JTextField(30);
+  JTextField yValues = new JTextField(30);
+
+  JLabel xLabel = new JLabel("X Axis Values:");
+  JLabel yLabel = new JLabel("Y Axis Values:");
+
+  JFrame dataFrame = new JFrame();
+
