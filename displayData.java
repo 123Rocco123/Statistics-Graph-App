f99547a75b30a23385a9147be1e4a8aa7817d79e
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-08-18 23:49:37 +0100

    Update displayData.java

--- displayData.java
+++ displayData.java
@@ -4,8 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+// Chart Information
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+
+import org.jfree.data.general.DefaultPieDataset;
+
 class displayData {
   public displayData() {
-    System.out.println("test");
+
   }
 }
